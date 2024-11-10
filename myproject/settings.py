3ce887845de6dbf3181f7ddb2f358ed4fd8a875c
Faisal Sami <124456301+fahimfaisalsami@users.noreply.github.com>
2024-11-11 04:01:21 +0600

    Add files via upload

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z%72pdmj6lcym$0+9o^7(94mqrcv=m0_-l+l$8f8$4-mdtz&t3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
